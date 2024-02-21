a5c2b05c625282ab60d66fbb36b6f6cd24bb3022
Preston Weight <psweight@gmail.com>
2024-02-21 12:10:59 -0700

    saving testing states

--- Program.cs
+++ Program.cs
@@ -8,12 +8,13 @@ class Program
         var order = new Order
         {
             CouponDiscount = 1000,
-            Discounts = [new Discount { CouponDiscount = 1000, EligibleItems=["Win", "Switch"] }, new Discount { CouponDiscount = 1000, EligibleItems=["Win", "Galvan"]}],
+            // Discounts = [new Discount { CouponDiscount = 1000, EligibleItems=["Win", "Switch"] }, new Discount { CouponDiscount = 1000, EligibleItems=["Win", "Galvan"]}],
+            Discounts = [new Discount { CouponDiscount = 20, EligibleItems=["Win", "Switch"] }, new Discount { CouponDiscount = 10, EligibleItems=["Win", "Switch"]}],
             OrderItems =
             [
-                new OrderItem { Name = "Win", LineTotal = 1000, SingleItemPrice = 500, IsEligibleForDiscount = true, Quantity = 2},
-                new OrderItem { Name = "Switch", LineTotal = 2000, SingleItemPrice = 2000, IsEligibleForDiscount = true, Quantity = 1},
-                new OrderItem { Name = "Galvan", LineTotal = 2000, SingleItemPrice = 2000, IsEligibleForDiscount = false, Quantity = 1}
+                new OrderItem { Name = "Win", LineTotal = 50, SingleItemPrice = 1, IsEligibleForDiscount = true, Quantity = 50},
+                new OrderItem { Name = "Switch", LineTotal = 50, SingleItemPrice = 10, IsEligibleForDiscount = true, Quantity = 5},
+                new OrderItem { Name = "Element", LineTotal = 50, SingleItemPrice = 10, IsEligibleForDiscount = false, Quantity = 5}
             ]
         };
 
@@ -28,7 +29,7 @@ public class OrderItem
 {
     public string Name { get; set; } = default!;
     public decimal LineTotal { get; set; }
-    public decimal SingleItemPrice { get; set; }
+    public decimal SingleItemPrice { get; set; }    
     public bool IsEligibleForDiscount { get; set; }
     public int Quantity { get; set; }
 }
